fa3f5a64a54af6d6a6abd0a94517085e48fd5896
Keenan Lidral-Porter <klp2012@gmail.com>
2017-01-17 15:17:04 -0800

    [feat] matches GET requests against a regex to capture full path info

--- code/server.js
+++ code/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/urlinfo/v1/:url', (req, res) => {
-  const url = req.params.url;
+app.get(/urlinfo\/v1\/(.*):([0-9]{1,5})\/(.*)/, (req, res) => {
+  const hostname = req.params[0];
+  const port = req.params[1];
+  const path = req.params[2];
+  const url = `${hostname}:${port}/${path}`;
   const body = {
     safe: true,
     url,
