61606dbe50ed6e9337ec4d2b1b789762d4ac385b
Keenan Lidral-Porter <klp2012@gmail.com>
2017-01-17 15:42:48 -0800

    [feat] prevents automatic parsing of query string
    Whatever is passed as {original_path_and_querystring} in
    GET /urlinfo/v1/{hostname_and_port}/{original_path_and_query_string}
    is now passed along to the middleware.

--- code/server.js
+++ code/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.set('query parser', string => string);
+
 app.get(/urlinfo\/v1\/(.*):([0-9]{1,5})\/(.*)/, (req, res) => {
   const hostname = req.params[0];
   const port = req.params[1];
   const path = req.params[2];
-  const url = `${hostname}:${port}/${path}`;
+  const queryString = req.query;
+
+  let url = `${hostname}:${port}/${path}`;
+  if (queryString) {
+    url += `?${queryString}`;
+  }
   const body = {
     safe: true,
     url,
