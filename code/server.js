97413e681d5b049d69a4e5131d4a58bed8bb78fd
Keenan Lidral-Porter <klp2012@gmail.com>
2017-01-18 15:33:57 -0800

    [feat] queries cassandra with url information from GET request

--- code/server.js
+++ code/server.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 
 const authProvider = new Cassandra.auth.PlainTextAuthProvider(
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
-);
+  process.env.DB_PASSWORD);
 const db = new Cassandra.Client({
   contactPoints: [
     '40.69.136.174',
@@ -17,7 +16,7 @@ const db = new Cassandra.Client({
   authProvider,
 });
 const app = express();
-const dbQuery = 'SELECT url_id, filter_level, safe FROM urls WHERE url_id = ?';
+const dbQuery = 'SELECT url_id, filter_level, safe FROM urls WHERE hostname = ? AND port = ? AND path = ? AND query_string = ? ALLOW FILTERING';
 const Uuid = Cassandra.types.Uuid;
 
 app.set('query parser', string => string);
@@ -26,31 +25,17 @@ app.get(/urlinfo\/v1\/(.*):([0-9]{1,5})\/(.*)/, (req, res) => {
   const hostname = req.params[0];
   const port = req.params[1];
   const path = req.params[2];
-  const query_string = req.query;
+  const queryString = `?${req.query}`;
 
-  const url = {
-    hostname,
-    port,
-    path,
-    query_string,
-  };
-
-  console.log('querying db');
-  db.execute(dbQuery, ['6f83c85c-9264-48cf-b2b2-0326aff5f510'])
+  console.log(hostname, port, path, queryString);
+  db.execute(dbQuery, [hostname, port, path, queryString])
     .then((result) => {
-      console.log('back from db');
       const row = result.first();
-      console.log(row);
+      res.send(row);
     })
     .catch((err) => {
-      console.log(err);
+      res.status(500).send(err);
     });
-
-  const body = {
-    safe: true,
-    url,
-  };
-  res.send(body);
 });
 
 app.listen(3000);
